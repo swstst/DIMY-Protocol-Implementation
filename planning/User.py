7004565f535540e4793bd27bc450e2e279b7ff60
SuWingTang <su.w.tang@student.unsw.edu.au>
2026-03-11 21:20:02 +1100

    added diary

--- planning/User.py
+++ planning/User.py
@@ -1,8 +1,20 @@
+from secrets import randbits
+from Crypto.Protocol.SecretSharing import Shamir
+from Crypto.Cipher import AES
+# Need to use AES.EAX to ensure reconstruction of secret is successful
+import sys
+
 class User:
       def __init__(self, Dt, qbf: BloomFilter):
             self.cycle = Dt
             self.qbf = qbf
             self.dbfs = list[BloomFilters]
+            self.ephID = self.gen_ephID()
+
+      def gen_ephID(self) -> bytes:
+            # generate a random number as secret key. 
+            # result need to be 256-bits
+            ...
 
       def counter(self):
             # thread for the time every second
@@ -15,4 +27,12 @@ class User:
             # combine all dbfs to upload to cbf
             cbf.upload(self.dbfs)
 
-      
+
+# for unit testing
+if __name__ == '__main__':
+      user = User()
+
+      assert len(user.ephID) == 256, f"Error [User.py]: EphID need to be 256-bits, not {len(user.eph_ID)}"
+
+      # eg command line
+      # python3 file.py -t [] -k [] -n []
