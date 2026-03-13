bc79f21c0c4440313743b5843905aae3374fb492
kelvin <kelvinjyoga@gmail.com>
2026-03-14 00:40:44 +1100

    more updates

--- src/Client/background_process.py
+++ src/Client/background_process.py
@@ -21,7 +21,9 @@ class backgroundProcess():
         The thread process to generate EphIDs every t
         """
         while not self.stop_event.is_set():
-            gen_EphID()
+            self.curr_EphID = gen_EphID()
+            self.EphID_ready_event.set()
+
             self.stop_event.wait(self.EphID_time_var)
 
     def SharedSecret_gen_thread_process(self):
@@ -33,6 +35,8 @@ class backgroundProcess():
             self.EphID_ready_event.wait(self.EphID_time_var + 2)
 
             self.secrets = SharedSecret_gen() # needs to take the new EphID as input
+
+            self.EphID_ready_event.clear()
     
     def SharedSecret_Distribution(self):
         """
@@ -41,9 +45,17 @@ class backgroundProcess():
         pass
 
     def ID_processes(self):
+        """
+        Run all the threads
+        """
         EphID_gen_thread = threading.Thread(target=gen_EphID_every_t, daemon=True)
-
         SharedSecret_gen_thread = threading.Thread(target=SharedSecret_gen, daemon=True)
-
         SSS_thread = threading.Thread(target=SharedSecret_Distribution, daemon=True)
+
+        EphID_gen_thread.start()
+        SharedSecret_gen_thread.start()
+        SSS_thread.start()
+
+    def stop_all_processes(self):
+        self.stop_event.set()
 
