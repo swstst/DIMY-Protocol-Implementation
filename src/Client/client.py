627fc7f41a9bfc7fe6a9e946f344a9782756e449
SuWingTang <su.w.tang@student.unsw.edu.au>
2026-03-19 16:59:22 +1100

    task3a - rough

--- src/Client/client.py
+++ src/Client/client.py
@@ -10,13 +10,11 @@ UDP_IP = '127.0.0.1'
 UDP_RECV_PORT = 8000
 UDP_SEND_PORT = 8001
 class client:
-    # message drop probability {30, 40, 50, 60, 70}
     def __init__(self, t, k, n, p):
         assert(t in {15,18,21,24,27,30})
         assert(k >= 3)
         assert(n >= 5)
-        assert(p <= 100)
-        assert(p.isdigit())
+        assert(p in {30, 40, 50, 60, 70})
 
         self.time_cycle = t     
         self.k = k
@@ -27,14 +25,23 @@ class client:
 
         # UDP socket for shares broadcast
         self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+        
+        # start listening on receiving UDP port
+        self.udp_sock.bind((UDP_IP, UDP_RECV_PORT))
+        
         # simultaneous broadcast shares 
         self.broadcast_thread = threading.Thread(target=self.timer_3s, daemon=False)
-        self.broadcast_thread.start()
+        
+        # simultaneous UDP port listening
+        self.udp_recv_thread = threading.Thread(target=self.receiver, daemon=False)
 
-        # drop a message 
+        # the following need to be moved to some client main thread
+        self.broadcast_thread.start()
+        self.udp_recv_thread.start()
+        
 
     def broadcast_shares(self, shares):
+        
         i = 0
         
         while True:
@@ -55,7 +62,6 @@ class client:
         
         msg = b'Hi'
         
-        probability = random.random(0, 1)
 
         if probability*10 < self.p:
             # drop message
@@ -63,9 +69,24 @@ class client:
 
     def receiver(self):
         # Listening on UDP port for message drops and broadcast shares
-        ...
+        while True:
+            # temp buffer size is 1024 bytes
+            data, addr = self.udp_sock.recvfrom(1024)
+
+            if not data:
+                continue
+            
+            p = random.random(0, 1)
+                
+            if p * 10 < self.p:
+                print("message dropped")
+                continue
+            
+            # store the data somewhere
+            print(f"received data: {data}")
+
 
 if __name__ == '__main__':
     # Create client class
-    
+
     ...
