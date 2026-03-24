116527633e8d52705a2a576a0e650f77fad53c68
SuWingTang <su.w.tang@student.unsw.edu.au>
2026-03-24 12:46:21 +1100

    separated gen ECC key pair for testing and updated test1 to reflect changes to EphID return type

--- src/Client/test_t1.py
+++ src/Client/test_t1.py
@@ -14,24 +14,27 @@ def t15_k3_n5_id() -> None:
       # test private key generation
       print()
       print("="*3, "PRIVATE KEY PEM format", "="*3)
-      priv_key = eph_id.export_key(format="PEM")
+      d = getrandbits(256)
+      keypair = gen_keyPair(d)
+      priv_key = keypair.export_key(format="PEM")
       print(priv_key)
       print()
 
       # verify SSS
+      eph_id = keypair.pointQ.x
       shares = SharedSecret_gen(eph_id, k, n)
       assert len(shares) == n, f"ID.py Err: {n} shares required but only got {len(shares)}"
       
       # test reconstruction of secrets with n-n shares
-      _n_n_shares = verify_k_of_n_shares_reconstruction(shares, (eph_id.pointQ.x).to_bytes(), n, n)
+      _n_n_shares = verify_k_of_n_shares_reconstruction(shares, eph_id.to_bytes(), n, n)
       assert (_n_n_shares == True), "ID.py Err: shares not constructed correctly"
       
       # test reconstruction of secrets with insufficient shares
-      _i_n_shares = verify_k_of_n_shares_reconstruction([shares[1], shares[3]],(eph_id.pointQ.x).to_bytes(), 2, n)
+      _i_n_shares = verify_k_of_n_shares_reconstruction([shares[1], shares[3]], eph_id.to_bytes(), 2, n)
       assert (_i_n_shares == False), "ID.py Err: shares not constructed correctly"
 
       # test reconstruction of secrets with k-n shares
-      _k_n_shares = verify_k_of_n_shares_reconstruction(shares[:k], (eph_id.pointQ.x).to_bytes(), k, n)
+      _k_n_shares = verify_k_of_n_shares_reconstruction(shares[:k], eph_id.to_bytes(), k, n)
       assert (_k_n_shares == True), "ID.py Err: shares not constructed correctly"
 
 
