58f0ff382ac8291244d5b0860128ffdfbcb6fe6b
SuWingTang <su.w.tang@student.unsw.edu.au>
2026-04-04 09:37:58 +1100

    client-side tcp sending logic complete. to be tested

---
+++ src/ephID/ID.py
@@ -0,0 +1,109 @@
+from Crypto.Random.random import getrandbits
+from Crypto.Protocol.SecretSharing import Shamir
+from Crypto.PublicKey import ECC
+from shamir import Shares, split, combine
+import math
+
+def gen_keyPair(d: int):
+    """
+    Generate ECC key pair for Ephemeral IDs.
+
+    args:
+        d ==> random 256-bit secret
+
+    returns:
+        keyPair ==> ECC key pair 
+        
+    """
+    keyPair = ECC.construct(curve='p256', d=d)
+    return keyPair
+
+def gen_EphID(t: int):
+    """
+    Generate Ephemeral IDs
+
+    args:
+        t ==> time to seed randomness (if possible)
+
+    returns:
+        Eph_ID ==> the resulting ephemeral ID (public key of ECC key pair)
+    """
+    # use t to seed the randomness (?)
+    
+    # secret - could use secrets instead of PyCrypto's random lib, however didn't find anything bad about it ...
+    d = getrandbits(256)
+    
+    EphID = gen_keyPair(d).pointQ.x
+
+    return EphID, d
+
+
+def gen_shares(new_EphID, k:int, n:int) -> tuple:
+    """
+    Generate k-out-of-n shamir shares
+
+    args:        new_EphID => the new ephemeral ID
+        k => the minimum amount of shares needed to reconstruct ID
+        n => ID is split into n shares
+    
+    return:
+        the shares back, the shares are in form (idx, share)
+    """
+
+    # convert ephID into bytes
+    eph_id_bytes = int(new_EphID).to_bytes(32, byteorder='big')
+    shares = split(secret = eph_id_bytes, parts = n, threshold = k)
+
+    return shares
+
+def combine_shares(shares: list) -> bytearray:
+    """
+    Reconstruct secret from k shares
+
+    args:
+        new_EphID => the new ephemeral ID
+        k => the minimum amount of shares needed to reconstruct ID
+        n => ID is split into n shares
+    
+    return:
+        the shares back, the shares are in form (idx, share)
+    """
+    
+    recovered = combine(shares)
+
+    return recovered
+
+def ECDH(pk, sk):
+    """
+    Elliptic Curve Diffie Hellman key exchange to get shared secret
+
+    args:
+        pk => public key of exchange buddy, in this case only the x of (x,y)
+        sk => private key
+
+    return:
+        sharedSecret.x => x point of the shared secret of the exchange
+    """
+    # FIPS 186-4 NIST prime
+    p = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
+
+    # FIPS constant
+    b = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
+
+    x = int(pk)
+    y_squared = (x**3 - 3*x + b) % p # p256 curve equation
+
+    # Tonelli-Shanks Algorithm to get Mod roots
+    y = pow(y_squared, (p + 1) // 4, p) # get the y back ( decompress )
+
+    #TODO confirm that this has neglible guessing advantage
+
+    assert((y * y) % p == y_squared)
+
+    # full public key
+    pub_key = ECC.construct(curve='p256', point_x=x, point_y=y).pointQ
+
+    sharedSecret = pub_key * sk
+
+    return sharedSecret
+
---
+++ src/ephID/test_t1.py
@@ -0,0 +1,59 @@
+from ID import *
+
+def t15_k3_n5_id() -> None:
+      t = 15
+      k = 3
+      n = 5
+      
+      keypair = gen_keyPair(d)
+
+      print("="*3, "PUBLIC KEY", "="*3)
+      print(f"Public Key (x): {hex(keypair.pointQ.x)}")
+      print(f"Public Key (y): {hex(keypair.pointQ.y)}")
+
+      # test private key generation
+      print()
+      print("="*3, "PRIVATE KEY PEM format", "="*3)
+      d = getrandbits(256)
+      priv_key = keypair.export_key(format="PEM")
+      print(priv_key)
+      print()
+
+      # verify SSS
+      eph_id = keypair.pointQ.x
+      shares = gen_shares(eph_id, k, n)
+      assert len(shares) == n, f"ID.py Err: {n} shares required but only got {len(shares)}"
+      
+      # test reconstruction of secrets with n-n shares
+      _n_n_shares = verify_k_of_n_shares_reconstruction(shares, eph_id.to_bytes(), n, n)
+      assert (_n_n_shares == True), "ID.py Err: shares not constructed correctly"
+      
+      # test reconstruction of secrets with insufficient shares
+      _i_n_shares = verify_k_of_n_shares_reconstruction([shares[1], shares[3]], eph_id.to_bytes(), 2, n)
+      assert (_i_n_shares == False), "ID.py Err: shares not constructed correctly"
+
+      # test reconstruction of secrets with k-n shares
+      _k_n_shares = verify_k_of_n_shares_reconstruction(shares[:k], eph_id.to_bytes(), k, n)
+      assert (_k_n_shares == True), "ID.py Err: shares not constructed correctly"
+
+
+def verify_k_of_n_shares_reconstruction(shares: list, eph_ID: bytes, i: int, n:int) -> bool:
+      
+      print("="*3, "SHARES RECEIVED", "="*3)
+      for share in shares:
+            print(share)
+      
+      print()
+      print('='*3, f"CONSTRUCT SECRET FROM {i}-of-{n} SHARES", "="*3)
+      recovered_secret = bytes(combine_shares(shares))
+
+      print(recovered_secret)
+      
+      print('='*3, "ORIGINAL SECRET", "="*3)
+      print(eph_ID)
+
+      return recovered_secret == eph_ID
+
+if __name__ == '__main__':
+      t15_k3_n5_id()
+      
