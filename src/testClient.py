594b1f780e88747612c366b9e0122cf50a779b1d
kelvin <kelvinjyoga@gmail.com>
2026-04-14 01:03:53 +1000

    protocol between nodes fixes

--- src/testClient.py
+++ src/testClient.py
@@ -1,5 +1,19 @@
-from Dimy import Client
+from ephID.ID import *
+from Crypto.Hash import SHA256
 
 if __name__ == '__main__':
-      client = Client(t=15, k=3, n=5, p=30)
-      client.run()
+        
+        ephid, d = gen_EphID()
+        new_shares = gen_shares(new_EphID=ephid, k=3, n=5)
+        shares = new_shares.copy()
+        shares = shares[:2] 
+        shares.append(new_shares[1])
+        print("SHARES: ", shares)
+        combined = combine_shares(shares)
+        
+
+        data = ephid.to_bytes(32, 'big')
+        print(ephid)
+        print("EPHID: ", SHA256.new(data=data).digest())
+        print(new_shares)
+        print("COMBINED: ", SHA256.new(data=combined).digest())
